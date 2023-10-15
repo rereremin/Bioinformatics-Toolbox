cb1c083e3e35dfe3a26ba47258a7226b8551add3
rereremin <114501294+rereremin@users.noreply.github.com>
2023-10-15 22:43:53 +0300

    Add convert_multiline_fasta_to_oneline()

--- bio_files_processor.py
+++ bio_files_processor.py
@@ -1,1 +1,20 @@
+import os
 
+def convert_multiline_fasta_to_oneline(input_fasta:str, output_fasta="") -> str:
+    with open(os.path.abspath(input_fasta)) as fasta_file:
+        
+        list_of_lines = []
+        for line in fasta_file.readlines():
+            if line.startswith(">"):
+                line = "\t"
+            list_of_lines.append(line)
+
+    longlines_to_file = "".join(list_of_lines[1:]).replace("\n", "").replace("\t", "\n")
+
+    if output_fasta == "":
+        raise ValueError("File hasn't got name!")
+    
+    output_file = output_fasta+".fasta"
+    with open(output_file, mode="w") as output_longline:
+        output_longline.write(longlines_to_file)
+        print(f"FASTA-seq write in {output_file} file")
