3ea1097e23c4bd7084dd9438a1e6fa4b8ccc754e
rereremin <114501294+rereremin@users.noreply.github.com>
2023-10-17 23:01:59 +0300

    Add select_genes_from_gbk_to_fasta() function

--- bio_files_processor.py
+++ bio_files_processor.py
@@ -18,3 +18,25 @@ def convert_multiline_fasta_to_oneline(input_fasta:str, output_fasta="") -> str:
     with open(output_file, mode="w") as output_longline:
         output_longline.write(longlines_to_file)
         print(f"FASTA-seq write in {output_file} file")
+
+def select_genes_from_gbk_to_fasta(input_gbk:str, genes:list, n_before=1, n_after=1, output_fasta="") -> str:
+    """
+    
+    """    
+    if output_fasta == "":
+        raise ValueError("File hasn't got name!")
+
+    ans = []
+    if type(genes) == list:
+        for gene in genes:
+            ans.append(select_flanking_genes(input_gbk, gene, n_before, n_after))
+    elif type(genes) == str:
+        ans.append(select_flanking_genes(input_gbk, genes, n_before, n_after))
+
+    output_file = output_fasta+".fasta"
+    with open(output_file, mode="w") as output_fasta:
+
+        for item in ans:
+            output_fasta.write("\n".join(item))
+            output_fasta.write('\n\n')
+        print(f"Flanking genes write in {output_file} file")
