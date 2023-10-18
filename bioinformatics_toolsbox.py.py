ead6cd76d9be851d1c1e70ad271708bb9aa68f81
rereremin <114501294+rereremin@users.noreply.github.com>
2023-10-18 16:07:59 +0300

    Add import EXAMPLE_FASTQ.py

--- bioinformatics_toolsbox.py.py
+++ bioinformatics_toolsbox.py.py
@@ -1,4 +1,5 @@
 from tools.fastq_filtration import filter_fastq
 from tools.protein_tools import run_protein_tools
 from tools.dna_rna_tools import run_dna_rna_tools
+from tools.EXAMPLE_FASTQ import EXAMPLE_FASTQ as ex_fastq
 
