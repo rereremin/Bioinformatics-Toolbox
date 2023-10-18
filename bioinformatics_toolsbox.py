d2cf337e91a760e19569b126290a3f73307915f8
rereremin <114501294+rereremin@users.noreply.github.com>
2023-10-18 18:28:55 +0300

    Import of bio_files_processor.py

--- bioinformatics_toolsbox.py
+++ bioinformatics_toolsbox.py
@@ -2,4 +2,5 @@ from tools.fastq_filtration import filter_fastq
 from tools.protein_tools import run_protein_tools
 from tools.dna_rna_tools import run_dna_rna_tools
 from tools.EXAMPLE_FASTQ import EXAMPLE_FASTQ as ex_fastq
+from tools.bio_files_processor import convert_multiline_fasta_to_oneline as cmf, select_genes_from_gbk_to_fasta as sgf
 
